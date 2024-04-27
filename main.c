ae7e89155acf73e9b4ab2131735c7093437ba62f
itsXrgon <ali.koheil@gmail.com>
2024-04-27 21:52:48 +0300

    add ALU functionality

--- main.c
+++ main.c
@@ -3,15 +3,18 @@
 #include <string.h>
 
 #define MAX_INSTRUCTION_LENGTH 100
-int data_memory[1000];
-int registers[16];
+int data_memory[2048][8];
+int generalRegisters[64];
+int SREG[5]; // SREG[0] = C, SREG[1] = V, SREG[2] = N, SREG[3] = S, SREG[4] = Z
 int pc = 0;
-int instruction_memory[1000];
+int instruction_memory[1024][16];
 
-void execute_instruction(const char* instruction) {
-    char* instruction_copy = strdup(instruction);
-    char* token = strtok(instruction_copy, " ");
-    if (strcmp(token, "ADD") == 0) {
+void execute_instruction(const char *instruction)
+{
+    char *instruction_copy = strdup(instruction);
+    char *token = strtok(instruction_copy, " ");
+    if (strcmp(token, "ADD") == 0)
+    {
         token = strtok(NULL, ", ");
         int dest_reg = atoi(token + 1);
         token = strtok(NULL, ", ");
@@ -21,7 +24,9 @@ void execute_instruction(const char* instruction) {
 
         int result = ALU(ReadRegister(src_reg1), ReadRegister(src_reg2), 0);
         WriteRegister(dest_reg, result);
-    } else if (strcmp(token, "SUB") == 0) {
+    }
+    else if (strcmp(token, "SUB") == 0)
+    {
         token = strtok(NULL, ", ");
         int dest_reg = atoi(token + 1);
         token = strtok(NULL, ", ");
@@ -31,7 +36,9 @@ void execute_instruction(const char* instruction) {
 
         int result = ALU(ReadRegister(src_reg1), ReadRegister(src_reg2), 1);
         WriteRegister(dest_reg, result);
-    } else if (strcmp(token, "MUL") == 0) {
+    }
+    else if (strcmp(token, "MUL") == 0)
+    {
         token = strtok(NULL, ", ");
         int dest_reg = atoi(token + 1);
         token = strtok(NULL, ", ");
@@ -41,7 +48,9 @@ void execute_instruction(const char* instruction) {
 
         int result = ALU(ReadRegister(src_reg1), ReadRegister(src_reg2), 2);
         WriteRegister(dest_reg, result);
-    } else if (strcmp(token, "DIV") == 0) {
+    }
+    else if (strcmp(token, "DIV") == 0)
+    {
         token = strtok(NULL, ", ");
         int dest_reg = atoi(token + 1);
         token = strtok(NULL, ", ");
@@ -51,62 +60,90 @@ void execute_instruction(const char* instruction) {
 
         int result = ALU(ReadRegister(src_reg1), ReadRegister(src_reg2), 3);
         WriteRegister(dest_reg, result);
-    } else if (strcmp(token, "AND") == 0) {
+    }
+    else if (strcmp(token, "AND") == 0)
+    {
         token = strtok(NULL, ", ");
         int dest_reg = atoi(token + 1);
         token = strtok(NULL, ", ");
         int src_reg1 = atoi(token + 1);
-        token = strtok(NULL , ", ");
+        token = strtok(NULL, ", ");
         int src_reg2 = atoi(token + 1);
     }
 }
-int ReadDataMemory(int address) {
+
+int ReadDataMemory(int address)
+{
     return data_memory[address];
 }
-void WriteDataMemory(int address, int value) {
+
+void WriteDataMemory(int address, int value)
+{
     data_memory[address] = value;
 }
-int ReadRegister(int reg) {
+
+int ReadRegister(int reg)
+{
     return registers[reg];
 }
-void WriteRegister(int reg, int value) {
+
+void WriteRegister(int reg, int value)
+{
     registers[reg] = value;
 }
-int ReadInstructionMemory(int address) {
+
+int ReadInstructionMemory(int address)
+{
     return instruction_memory[address];
 }
-void WriteInstructionMemory(int address, int value) {
+
+void WriteInstructionMemory(int address, int value)
+{
     instruction_memory[address] = value;
 }
-void IncrementPC() {
+
+void IncrementPC()
+{
     pc++;
 }
-void SetPC(int value) {
+
+void SetPC(int value)
+{
     pc = value;
 }
-int GetPC() {
+
+int GetPC()
+{
     return pc;
 }
-void ResetProcessor() {
-    for (int i = 0; i < 1000; i++) {
+
+void ResetProcessor()
+{
+    for (int i = 0; i < 1000; i++)
+    {
         data_memory[i] = 0;
         instruction_memory[i] = 0;
     }
-    for (int i = 0; i < 16; i++) {
+    for (int i = 0; i < 16; i++)
+    {
         registers[i] = 0;
     }
     pc = 0;
 }
-void LoadProgram(const char* file_name) {
-    FILE* file = fopen(file_name, "r");
-    if (file == NULL) {
+
+void LoadProgram(const char *file_name)
+{
+    FILE *file = fopen(file_name, "r");
+    if (file == NULL)
+    {
         printf("Failed to open the program file.\n");
         return;
     }
 
     int address = 0;
     char line[MAX_INSTRUCTION_LENGTH];
-    while (fgets(line, sizeof(line), file)) {
+    while (fgets(line, sizeof(line), file))
+    {
         // Remove the newline character from the end of the instruction
         line[strcspn(line, "\n")] = '\0';
 
@@ -117,46 +154,147 @@ void LoadProgram(const char* file_name) {
     fclose(file);
 }
 
-int ALU (int operand1, int operand2, int operation) {
-    switch (operation) {
-        case 0: // ADD
-            return operand1 + operand2;
-        case 1: // SUB
-            return operand1 - operand2;
-        case 2: // MUL
-            return operand1 * operand2;
-        case 3: // DIV
-            return operand1 / operand2;
-        case 4: // AND
-            return operand1 & operand2;
-        case 5: // OR
-            return operand1 | operand2;
-        case 6: // XOR
-            return operand1 ^ operand2;
-        case 7: // NOT
-            return ~operand1;
-        case 8: // SHL
-            return operand1 << operand2;
-        case 9: // SHR
-            return operand1 >> operand2;
-        case 10: // MOD
-            return operand1 % operand2;
-        case 11: // CMP
-            return operand1 - operand2;
-        default:
-            return 0;
+void updateCarryFlag(int operand1, int operand2, int result)
+{
+    if (result < operand1 || result < operand2)
+    {
+        SREG[0] = 1;
+    }
+    else
+    {
+        SREG[0] = 0;
     }
 }
 
+void updateOverflowFlag(int operand1, int operand2, int result)
+{
+    if (operand1 > 0 && operand2 > 0 && result < 0)
+    {
+        SREG[1] = 1;
+    }
+    else if (operand1 < 0 && operand2 < 0 && result > 0)
+    {
+        SREG[1] = 1;
+    }
+    else
+    {
+        SREG[1] = 0;
+    }
+}
 
+void updateNegativeFlag(int result)
+{
+    if (result < 0)
+    {
+        SREG[2] = 1;
+    }
+    else
+    {
+        SREG[2] = 0;
+    }
+}
 
-int main() {
+void updateSignFlag(int result)
+{
+    if (result == 0)
+    {
+        SREG[3] = 1;
+    }
+    else
+    {
+        SREG[3] = 0;
+    }
+}
+
+void updateZeroFlag(int result)
+{
+    if (result == 0)
+    {
+        SREG[4] = 1;
+    }
+    else
+    {
+        SREG[4] = 0;
+    }
+}
+
+int ALU(int operand1, int operand2, int operation)
+{
+    int result;
+    switch (operation)
+    {
+    case 0: // ADD
+        result = operand1 + operand2;
+        updateCarryFlag(operand1, operand2, result);
+        updateOverflowFlag(operand1, operand2, result);
+        updateNegativeFlag(result);
+        updateSignFlag(result);
+        updateZeroFlag(result);
+        return result;
+    case 1: // SUB
+        result = operand1 - operand2;
+        updateOverflowFlag(operand1, operand2, result);
+        updateNegativeFlag(result);
+        updateSignFlag(result);
+        updateZeroFlag(result);
+        return result;
+    case 2: // MUL
+        out = operand1 * operand2;
+        updateNegativeFlag(result);
+        updateZeroFlag(result);
+        return result;
+    case 3: // Move immediate
+        WriteRegister(operand1, operand2);
+        return 0;
+    case 4: // Branch if equal zero
+        if (operand1 == 0)
+        {
+            SetPC(operand2);
+        }
+        return 0;
+    case 5: // AND Immediate
+        result = operand1 & operand2;
+        updateNegativeFlag(result);
+        updateZeroFlag(result);
+        return result;
+    case 6: // Exclusive OR
+        result = operand1 ^ operand2;
+        updateNegativeFlag(result);
+        updateZeroFlag(result);
+    case 7: // Branch Register
+        SetPC(operand1 || operand2);
+        return 0;
+    case 8: // Shift Arithmetic Left
+        result = operand1 << operand2;
+        updateNegativeFlag(result);
+        updateZeroFlag(result);
+        return result;
+    case 9: // Shift Arithmetic Right
+        result = operand1 >> operand2;
+        updateNegativeFlag(result);
+        updateZeroFlag(result);
+        return result;
+    case 10: // Load to Register
+        WriteRegister(operand1, ReadDataMemory(operand2));
+        return 0;
+    case 11: // Store from Register
+        WriteDataMemory(operand1, ReadRegister(operand2));
+        return operand1;
+    default:
+        return 0;
+    }
+}
+
+int main()
+{
     ResetProcessor();
     LoadProgram("assembly.txt");
 
-    while (1) {
+    while (1)
+    {
         int instruction = ReadInstructionMemory(GetPC());
-        if (instruction == 0) {
+        if (instruction == 0)
+        {
             break;
         }
 
